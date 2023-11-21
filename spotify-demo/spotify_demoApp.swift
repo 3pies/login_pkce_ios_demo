4242a32e8827c39a41b909159454ce262c7eda40
danielferrer <danybarco@gmail.com>
2023-11-21 07:42:02 +0100

    Initial Commit

---
+++ spotify-demo/spotify_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotify_demoApp.swift
+//  spotify-demo
+//
+//  Created by Daniel Ferrer on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct spotify_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
